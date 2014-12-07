3e0c1673c50ad37b9fa5ad91d8cc64f484e5bec4
viviakemi <vivi_akemi@yahoo.com.br>
2014-12-07 17:18:44 -0200

    conta listar e incluir

--- ControleBancario/src/br/com/ufscar/controlebancario/server/ControleBancarioJdoDAO.java
+++ ControleBancario/src/br/com/ufscar/controlebancario/server/ControleBancarioJdoDAO.java
@@ -1,7 +1,12 @@
 package br.com.ufscar.controlebancario.server;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Query;
 
 import org.hibernate.Session;
 
@@ -9,6 +14,7 @@ import br.com.ufscar.controlebancario.client.ControleBancarioDAO;
 import br.com.ufscar.controlebancario.server.util.HibernateUtil;
 import br.com.ufscar.controlebancario.shared.Banco;
 import br.com.ufscar.controlebancario.shared.Conta;
+import br.com.ufscar.controlebancario.shared.ContaCliente;
 
 public class ControleBancarioJdoDAO implements ControleBancarioDAO{
 
@@ -49,7 +55,16 @@ public class ControleBancarioJdoDAO implements ControleBancarioDAO{
 	public void addConta(Conta conta) {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 	    session.beginTransaction();
-	    session.saveOrUpdate(conta);
+	    session.save(conta);
+	    System.out.println(conta.getIdConta());
+	    
+	    Set<ContaCliente> contaCliente = conta.getContaCliente();
+	    Iterator<ContaCliente> i = contaCliente.iterator();
+	    while(i.hasNext()){
+	    	ContaCliente cc = i.next();
+	    	cc.setConta(conta);
+	    	session.save(cc);
+	    }
 	    session.getTransaction().commit();
 	}
 
@@ -57,10 +72,28 @@ public class ControleBancarioJdoDAO implements ControleBancarioDAO{
 	public List<Conta> listConta() {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
-		List<Conta> contas = new ArrayList<Conta>(session.createQuery("from Conta").list());
+		
+		String hql = "FROM Conta ";
+		org.hibernate.Query query = session.createQuery(hql);
+		List<Conta> contas = query.list();
+		List<Conta> contas2 = new ArrayList<Conta>();
+		for(Conta conta:contas){
+			Conta contaAux = new Conta(conta.getIdConta(), conta.getTipoConta(), conta.getNumero(),
+					conta.getDataAbertura(), conta.getSaldo(), conta.getContaCliente());
+			Set<ContaCliente> ccAux = conta.getContaCliente();
+			Set<ContaCliente> ccAux2 = new HashSet<ContaCliente>();
+			Iterator<ContaCliente> i = ccAux.iterator();
+			while (i.hasNext()){
+				ContaCliente cc = i.next();
+				ccAux2.add(new ContaCliente(cc.getIdContaCliente(), contaAux, cc.getIdCliente(), cc.getTitular()));
+				
+			}
+			contaAux.setContaCliente(ccAux2);
+			contas2.add(contaAux);
+		}
 		
 		session.getTransaction().commit();
-		return contas;
+		return contas2;
 	}
 
 }
